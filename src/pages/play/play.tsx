7a32b26c27dd377a154b38b602ea1a0c2fdcb0b5
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-05-03 22:46:14 +0900

    re first

---
+++ src/pages/play/play.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import YouTube, { YouTubeProps } from "react-youtube";
+
+// type Props = {
+//   videoId: string;
+// };
+
+// function Play(props: Props) {
+function Play() {
+  const [YTPlayer, setYTPlayer] = useState<YT.Player>();
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    setYTPlayer(event.target);
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <>
+      <YouTube videoId="N6di9-zLGbw" opts={opts} onReady={onPlayerReady} />
+      <button
+        onClick={() => {
+          if (YTPlayer) {
+            YTPlayer.playVideo();
+          }
+        }}
+      >
+        再生
+      </button>
+      <button
+        onClick={() => {
+          if (YTPlayer) {
+            YTPlayer.pauseVideo();
+          }
+        }}
+      >
+        停止
+      </button>
+    </>
+  );
+}
+export default Play;
